42f3bde51a109a2c00a4f3cd1170b805f7a27839
shatunelly7 <shatunelly7@gmail.com>
2022-03-28 13:04:55 +0300

    jsassignment

---
+++ conditions.js
@@ -0,0 +1,10 @@
+let temperature=60;
+if(temperature>=20 && temperature<=36){
+    console.log("normal body temperature");
+}
+else if(temperature>=37 && temperature<=40){
+
+}
+else{console.log("temperature very high"); 
+}
+
